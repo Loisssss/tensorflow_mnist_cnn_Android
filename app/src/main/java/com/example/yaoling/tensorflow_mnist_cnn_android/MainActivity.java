333bee46dc83de8cbdddedc2f24eb9e0f47be2f8
yaoling <245807889lois@gmail.com>
2019-01-29 10:26:48 +0000

    finish clear function and predict function

--- app/src/main/java/com/example/yaoling/tensorflow_mnist_cnn_android/MainActivity.java
+++ app/src/main/java/com/example/yaoling/tensorflow_mnist_cnn_android/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.yaoling.tensorflow_mnist_cnn_android;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.example.yaoling.tensorflow_mnist_cnn_android.DrawView;
+import android.content.res.AssetManager;
+
 
 public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
     private TextView textViewProbability;
     private Button buttonClear;
     private Button buttonPredict;
-    private  DrawView drawView;
+    private DrawView drawView;
     private TFClassifier tfClassifier;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +29,16 @@ public class MainActivity extends AppCompatActivity {
         textViewProbability = findViewById(R.id.textViewProbability);
         buttonClear = findViewById(R.id.buttonClear);
         buttonPredict = findViewById(R.id.buttonPredict);
+        drawView = findViewById(R.id.drawView);
+        tfClassifier =new TFClassifier(getAssets(), "file:///android_asset/model.pb");//输入模型存放路径，并加载TensoFlow模型
+
 
         buttonClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 drawView.clearCanvas();
+                textViewResult.setText("Model Prediction: ");
+                textViewProbability.setText("Confidence: ");
             }
         });
 
--- app/src/main/java/com/example/yaoling/tensorflow_mnist_cnn_android/TFClassifier.java
+++ app/src/main/java/com/example/yaoling/tensorflow_mnist_cnn_android/TFClassifier.java
@@ -1,27 +1,41 @@
 package com.example.yaoling.tensorflow_mnist_cnn_android;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.util.Log;
 
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
+import static android.support.constraint.Constraints.TAG;
+
+
 public class TFClassifier {
+
+    private DrawView drawView;
     static {
         System.loadLibrary("tensorflow_inference");
+        Log.e(TAG,"libtensorflow_inference.so库加载成功");
+
     }
 
     private static final int SIZE = 28 * 28;
 
-    private TensorFlowInferenceInterface inferenceInterface;
+    private final TensorFlowInferenceInterface inferenceInterface;
     private static final String MODEL_FILE = "model.pb";
-    private static final String INPUT_NODE = "x";
+    private static final String INPUT_NODE = "input/x_input";
     private static final String[] OUTPUT_NODES = {"logits_eval"};
     private static final String OUTPUT_NODE = "logits_eval";
     private static final long[] INPUT_SIZE = {1,SIZE};
     private static final int OUTPUT_SIZE = 10;
 
-    public TFClassifier(Context context) {
-        inferenceInterface = new TensorFlowInferenceInterface(context.getAssets(), MODEL_FILE);
-
+    
+    public TFClassifier(AssetManager assetManager, String modePath) {
+        //初始化TensorFlowInferenceInterface对象
+        inferenceInterface = new TensorFlowInferenceInterface(assetManager,modePath);
+        Log.e(TAG,"TensoFlow模型文件加载成功");
     }
 
     public float[] predict(float[] data) {
@@ -31,5 +45,4 @@ public class TFClassifier {
         inferenceInterface.fetch(OUTPUT_NODE, result);
         return result;
     }
-
 }
